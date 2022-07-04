8e135a12162f63646bbe56ca14ce7c013ebdbf4c
Adriaan van Rossum <1079135+adriaanvanrossum@users.noreply.github.com>
2022-07-04 10:58:38 +0200

    Add 404 page

---
+++ pages/404.vue
@@ -0,0 +1,14 @@
+<template>
+  <NuxtLayout name="default">
+    <div class="text-center mx-auto">
+      <p>Oops, page not found.</p>
+    </div>
+  </NuxtLayout>
+</template>
+
+<script setup>
+definePageMeta({
+  layout: false,
+  title: "Page not found",
+});
+</script>
